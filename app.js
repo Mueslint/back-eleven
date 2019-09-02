e01fd2bb7e66c9f96ceb8a1e2b2aacb0969c3582
Mueslint <solom.emmanuel@gmail.com>
2019-09-02 11:49:05 +0200

    Add delete and update route

--- app.js
+++ app.js
@@ -18,9 +18,15 @@ const firebaseConfig = {
     messagingSenderId: "1056914285093",
     appId: "1:1056914285093:web:eb307ef2bb57ff0c"
   };
+// Init Firebase DB connection
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore()
+
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
+
+// Avoid CORS related error
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -28,17 +34,17 @@ app.use(function(req, res, next) {
 });
 
 
-firebase.initializeApp(firebaseConfig);
 
-let db = firebase.firestore()
-
+// Fetch all document in astronaut collection
 app.get("/getAstronauts", (req, res) => {
   db.collection('astronauts').get()
     .then((snapshot) => {
       const astronauts = []
 
       snapshot.forEach(doc => {
-        astronauts.push(doc.data());
+        const astronaut = doc.data();
+        astronaut.ref = doc.id;
+        astronauts.push(astronaut);
       });
 
       res.status(200).send(astronauts);
@@ -49,6 +55,7 @@ app.get("/getAstronauts", (req, res) => {
     })
 })
 
+// create a db document in astronaut collection
 app.post("/postNewAstronaut", (req, res) => {
   db.collection('astronauts').add(req.body)
   .then(() => {
@@ -56,6 +63,43 @@ app.post("/postNewAstronaut", (req, res) => {
   })
 })
 
+app.post("/deleteAstronaut", (req, res) => {
+  const ref = req.body.ref
+  db.collection('astronauts').where(firebase.firestore.FieldPath.documentId(), "==", ref).get()
+    .then( snapshot => {
+      if(snapshot.docs.length == 1) {
+        snapshot.docs[0].ref.delete()
+        res.status(200).send("doc deleted")
+      } else {
+        console.error("sameKeyError: 0 or more than one document has been found for this doc.id");
+        res.status(500).send("Error in query")
+      }
+    })
+    .catch(err => {
+      console.error('Error fetching documents', err);
+      res.status(500).send('Error fetching documents')
+    })
+});
+
+app.post("/updateAstronaut", (req, res) => {
+  const updatedAstronaut = req.body.updatedAstronaut
+  const ref = req.body.ref
+
+  db.collection('astronauts').where(firebase.firestore.FieldPath.documentId(), "==", ref).get()
+    .then( snapshot => {
+      if(snapshot.docs.length == 1) {
+        snapshot.docs[0].ref.update(updatedAstronaut)
+        res.status(200).send("doc updated")
+      } else {
+        console.error("sameKeyError: 0 or more than one document has been found for this doc.id");
+        res.status(500).send("Error in query")
+      }
+    })
+    .catch(err => {
+      console.error('Error fetching documents', err);
+      res.status(500).send('Error fetching documents')
+    })
+});
 
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
